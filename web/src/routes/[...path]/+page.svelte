b30c56512c5120e9a266e4e4de3206bdd2683bce
hybridherbst <felix.herbst@gmail.com>
2024-04-08 09:46:22 +0200

    distinguish between directories and child items

--- web/src/routes/[...path]/+page.svelte
+++ web/src/routes/[...path]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { base } from '$app/paths';
-import type { HierarchyEntry } from '$lib/files';
+import type { HierarchyEntry, HierarchyItem } from '$lib/files';
+  import { onMount } from 'svelte';
 import GridItem from '../GridItem.svelte';
 export let data;
 
@@ -14,16 +15,41 @@ function getChilds() {
     return childItems;
 }
 
-$: current = getChilds().find(child => child.path === data.slug);
+function getItems() {
+    const items = new Array<HierarchyItem>();
+    const addLevel = ((entry: HierarchyEntry) => {
+        items.push(...entry.items);
+        entry.children.forEach(child => addLevel(child));
+    });
+    addLevel(data.posts);
+    return items;
+}
+
+onMount(() => {
+    dirs = getChilds();
+    items = getItems();
+});
+
+let dirs: HierarchyEntry[];
+let items: HierarchyItem[];
+
+$: currentDir = dirs?.find(child => child.path === "/" + data.slug);
+$: currentItem = items?.find(item => item.path === "/" + data.slug);
 
 </script>
 
 <div>
 <a href={base}>See all assets</a>
+<p>{data.slug} --- {currentDir?.path}</p>
 
-{#if current}
+{#if currentDir}
     <article>
-        <GridItem child={current} />
+        <GridItem child={currentDir} />
+    </article>
+{:else if currentItem}
+    <article>
+        <p>{JSON.stringify(currentItem, null, 2)}</p>
+        <a href={base}{currentItem.path}>Download</a>
     </article>
 {:else}
 <code><pre>
